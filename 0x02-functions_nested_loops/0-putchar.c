3f9d771139a124aacf697210102a0f8c2c04744f
Sammy Munala <sammy.munala@outlook.com>
2023-08-15 11:24:54 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "_putcher.h"
+/**
+ * main - Entry point
+ *
+ * Return: Alway 0 (Sucess)
+ */
+
+int main(void)
+{
+	char *h = "_putchar\n";
+
+	while (*h)
+	{
+		_putchar(*h);
+		h++;
+	}
+	return (0);
+}
