99c4d5ace47caa07e15094e0226e9d1ca9e831ed
Sammy Munala <sammy.munala@outlook.com>
2023-08-16 02:59:00 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "_putchar.h"
 /**
  * main - Entry point
  *
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-	char *h = "Holberton\n";
+	char *h = "_putchar\n";
 
 	while (*h)
 	{
